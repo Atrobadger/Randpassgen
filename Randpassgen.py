8edabd4c53356ed63c37b7390a7610fff6face72
Atrobadger <42151232+Atrobadger@users.noreply.github.com>
2022-11-11 23:57:21 +1100

    Create Randpassgen.py

---
+++ Randpassgen.py
@@ -0,0 +1,18 @@
+import random
+
+charArray = ['A','a','B','b','C','c','D','d','E','e','F','f','G','g','H','h',
+             'I','i','J','j','K','k','L','l','M','m','N','n','O','o','P','p',
+             'Q','q','R','r','S','s','T','t','U','u','V','v','W','w','X','x',
+             'Y','y','Z','z','0','1','2','3','4','5','6','7','8','9','!','@',
+             '#','$','%','^','&','*','.']
+
+passwordLength = input("Enter a number for password length: ")
+passwordSize = int(passwordLength)
+arrayLength = len(charArray)
+password = ''
+
+for i in range(0, passwordSize):
+    randNumber = random.randint(0, arrayLength)
+    randChar = charArray[randNumber]
+    password = password + randChar
+print("The generated password is " + password)
